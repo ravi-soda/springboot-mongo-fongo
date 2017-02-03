3bc8187b261b4414ab6dea57c6dcefeff68bb1ce
ravi-soda <raviprasad38@gmail.com>
2017-02-03 07:17:26 +0530

    Mocking PersonRepository class

--- src/main/test/java/in/ravikalla/config/TestConfiguration.java
+++ src/main/test/java/in/ravikalla/config/TestConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages={    
-    "in.ravikalla"})
+    "in.ravikalla.controllers", "in.ravikalla.service"})
 public class TestConfiguration {
   
-  
-  
+  @Bean
+  public PersonRepository getPersonRepositoryMock(){
+    return Mockito.mock(PersonRepository.class);
+  }  
 }
